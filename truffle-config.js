7cfda6ea569cb5a84c7664dbf3a4a339b4c83e92
dmitrylavrenov <lawrenowdima@gmail.com>
2022-01-21 17:28:28 +0300

    Change ChainId

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,7 @@ const solidityCompilerVersion = '^0.7.0';
 const privateKeyDev =
     '99b3c12287537e38c90a9219d4cb074a89a16e9cdb20bf85728ebd97c343e342';
 const humanodeHost = 'http://localhost:9933/';
-const humanodeChainId = 2020;
+const humanodeChainId = 42;
 
 module.exports = {
     networks: {
