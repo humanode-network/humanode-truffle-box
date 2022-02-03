bc60f7c85455adb80b7646349ab3bd127d756bb4
dmitrylavrenov <lawrenowdima@gmail.com>
2022-02-03 16:46:36 +0300

    Apply editorconfig

--- README.md
+++ README.md
@@ -8,14 +8,14 @@ Truffle predefined setup to deploy and test Ethereum smart contracts on Humanode
 
 2. With the files in your local system, the next step is to install all dependencies by running:
 
-`npm install`
+   `npm install`
 
 3. Compile and tests contracts.
 
-`./node_modules/.bin/truffle compile` - to compile
+   `./node_modules/.bin/truffle compile` - to compile
 
-`./node_modules/.bin/truffle test` - to test
+   `./node_modules/.bin/truffle test --network dev` - to test
 
 4. Deploy to Humanode network.
 
-`./node_modules/.bin/truffle migrate --network dev`
+   `./node_modules/.bin/truffle migrate --network dev`
--- truffle-config.js
+++ truffle-config.js
@@ -3,31 +3,26 @@ const solidityCompilerVersion = '^0.7.0';
 
 // Humanode Development Node config data
 const privateKeyDev =
-    '99b3c12287537e38c90a9219d4cb074a89a16e9cdb20bf85728ebd97c343e342';
-const humanodeHost = 'http://localhost:9933/';
-const humanodeChainId = 42;
+  '99b3c12287537e38c90a9219d4cb074a89a16e9cdb20bf85728ebd97c343e342';
+const humanodeRpcUrl = 'http://localhost:9933/';
+const humanodeChainId = 2020;
 
 module.exports = {
-    networks: {
-        // Humanode Development Network
-        dev: {
-            provider: () => {
-                if (!privateKeyDev.trim()) {
-                    throw new Error(
-                        'Please enter a private key with funds, you can use the default one'
-                    );
-                }
-                return new HDWalletProvider(
-                    privateKeyDev,
-                    humanodeHost
-                );
-            },
-            network_id: humanodeChainId,
-        },
+  networks: {
+    // Humanode Development Network
+    dev: {
+      provider: () => {
+        return new HDWalletProvider(
+          privateKeyDev,
+          humanodeRpcUrl
+        );
+      },
+      network_id: humanodeChainId,
     },
-    compilers: {
-        solc: {
-            version: solidityCompilerVersion,
-        },
-    }
+  },
+  compilers: {
+    solc: {
+      version: solidityCompilerVersion,
+    },
+  }
 };
