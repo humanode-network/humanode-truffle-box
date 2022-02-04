f3c42490c7b98f261f2d07b62b78acf4726632c7
dmitrylavrenov <lawrenowdima@gmail.com>
2022-02-04 12:02:02 +0300

    Support test on humanode testnet

--- test/test_MyToken.js
+++ test/test_MyToken.js
@@ -1,4 +1,4 @@
-// Example test script - Uses Mocha and Ganache
+// Example test script
 const Token = artifacts.require("MyToken");
 
 contract('MyToken', accounts => {
@@ -16,34 +16,46 @@ contract('MyToken', accounts => {
 
   // Check the balance of the owner of the contract
   it("should return the balance of token owner", async () => {
-    const balance = await token.balanceOf.call(accounts[0]);
+    // Prepare test data
+    const alice = accounts[0];
+
+    // Check balance
+    const balance = await token.balanceOf.call(alice);
     assert.equal(balance, _totalSupply, 'balance is wrong');
   });
 
   // Transfer token and check balances
   it("should transfer token", async () => {
+    // Prepare test data
     const amount = "1000000000000000000";
+    const alice = accounts[0];
+    const bob = accounts[1];
+
     // Transfer method
-    await token.transfer(accounts[1], amount, { from: accounts[0] });
-    balance1 = await token.balanceOf.call(accounts[1]);
-    assert.equal(balance1, amount, 'accounts[1] balance is wrong');
+    await token.transfer(bob, amount, { from: alice });
+    balanceBob = await token.balanceOf.call(bob);
+    assert.equal(balanceBob, amount, 'Bob balance is wrong');
   });
 
   // Set an allowance to an account, transfer from that account, check balances
   it("should give accounts[1] authority to spend accounts[0]'s token", async () => {
+    // Prepare test data
     const amountAllow = "10000000000000000000";
     const amountTransfer = "1000000000000000000";
+    const alice = accounts[0];
+    const bob = accounts[1];
+    const charlie = accounts[2];
 
-    // Approve accounts[1] to spend from accounts[0]
-    await token.approve(accounts[1], amountAllow, { from: accounts[0] });
-    const allowance = await token.allowance.call(accounts[0], accounts[1]);
+    // Approve Bob to spend from Alice
+    await token.approve(bob, amountAllow, { from: alice });
+    const allowance = await token.allowance.call(alice, bob);
     assert.equal(allowance, amountAllow, 'allowance is wrong');
 
     // Transfer tokens and check new balances
-    await token.transferFrom(accounts[0], accounts[2], amountTransfer, { from: accounts[1] });
-    const balance1 = await token.balanceOf.call(accounts[1]);
-    assert.equal(balance1, 0, 'accounts[1] balance is wrong');
-    const balance2 = await token.balanceOf.call(accounts[2]);
-    assert.equal(balance2, amountTransfer, 'accounts[2] balance is wrong');
+    await token.transferFrom(alice, charlie, amountTransfer, { from: bob });
+    const balanceBob = await token.balanceOf.call(bob);
+    assert.equal(balanceBob, 0, 'Bob balance is wrong');
+    const balanceCharlie = await token.balanceOf.call(charlie);
+    assert.equal(balanceCharlie, amountTransfer, 'Charlie balance is wrong');
   })
 });
